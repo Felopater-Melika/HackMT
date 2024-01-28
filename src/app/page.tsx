e38f3a10e52639c56c2e68642d7b6e6bed496411
Einar Strandberg <einargs@gmail.com>
2024-01-28 09:55:11 -0600

    change apostrophes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -37,17 +37,17 @@ export default function Home() {
         <p className="mb-10">
           BlueAid is a project designed to help with overly expensive medical
           bills. Hospitals cut deals with insurance companies to charge them
-          less money for the same procedure. Fortunately, if you're uninsured
-          and have a large bill, you can often negotiate with the hospital to
-          pay less.
+          less money for the same procedure. Fortunately, if you&apos;re
+          uninsured and have a large bill, you can often negotiate with the
+          hospital to pay less.
         </p>
 
         <p className="">
           This is where BlueAid helps: you take the itemized bill you recieve
           from the hospital, take a picture of it, and then upload it to our
-          site. We'll then use an open source database of different costs for
-          common procedures to look up what the normal price is and compare it
-          to what the hospital is charging you. You can then take that
+          site. We&apos;ll then use an open source database of different costs
+          for common procedures to look up what the normal price is and compare
+          it to what the hospital is charging you. You can then take that
           information into negotiations to ask for a lower price.
         </p>
       </div>
